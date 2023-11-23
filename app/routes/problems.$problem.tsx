1a02b46f0d9a8b848ca8752dae525da669d39d67
cdb61092 <36825464+cdb61092@users.noreply.github.com>
2023-11-23 16:32:52 -0600

    Add single problem page

---
+++ app/routes/problems.$problem.tsx
@@ -0,0 +1,28 @@
+import { CodeEditor } from "~/components/Editor";
+import {useLoaderData} from "@remix-run/react";
+import {prisma} from "~/utils/db.server";
+import {json} from "@remix-run/node";
+
+export async function loader({params}) {
+
+    console.log(params)
+    const problem = await prisma.problem.findUnique({
+        where: {
+            id: Number(params.problem)
+        }
+    });
+  return json({ problem });
+}
+
+
+export default function Problem() {
+    const { problem } = useLoaderData<typeof loader>();
+
+    // console.log(problem)
+  return (
+    <>
+      <h1>Problem</h1>
+        <CodeEditor code={problem?.starterCode ?? ''}/>
+    </>
+  );
+}
